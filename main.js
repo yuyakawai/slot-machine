10824f39222c687d94297a547b8e561baef5659b
administrator <register@yuyakawai.com>
2025-01-13 01:04:41 +0900

    refactor reel update logic to improve spinning behavior and cell position adjustment

--- main.js
+++ main.js
@@ -195,20 +195,50 @@ const scene = [
   {
     name: "gamePlay",
     update: () => {
-      reels[0].isSpinning = controller.buttons.find((e) => e.name === "left")
-        .isPressed
-        ? false
-        : true;
-      reels[1].isSpinning = controller.buttons.find((e) => e.name === "center")
-        .isPressed
-        ? false
-        : true;
-      reels[2].isSpinning = controller.buttons.find((e) => e.name === "right")
-        .isPressed
-        ? false
-        : true;
+      if (controller.buttons.find((e) => e.name === "left").isPressed) {
+        reels[0].isSpinning = false;
+        reels.map((reel) =>
+          reel.cells.map((cell) => {
+            if (cell.y % reel.cellHeight !== 0) {
+              cell.y--;
+            }
+          })
+        );
+      }
+
+      if (controller.buttons.find((e) => e.name === "center").isPressed) {
+        reels[1].isSpinning = false;
+        reels.map((reel) =>
+          reel.cells.map((cell) => {
+            if (cell.y % reel.cellHeight !== 0) {
+              cell.y--;
+            }
+          })
+        );
+      }
+
+      if (controller.buttons.find((e) => e.name === "right").isPressed) {
+        reels[2].isSpinning = false;
+        reels.map((reel) =>
+          reel.cells.map((cell) => {
+            if (cell.y % reel.cellHeight !== 0) {
+              cell.y--;
+            }
+          })
+        );
+      }
 
       drawReel();
+
+      if (reels.every((reel) => reel.isSpinning === false)) {
+        if (
+          reels.every((reel) =>
+            reel.cells.every((cell) => cell.y % reel.cellHeight === 0)
+          )
+        ) {
+          gameStatus.currentScene = scene.find((e) => e.name === "result");
+        }
+      }
     },
   },
   {
